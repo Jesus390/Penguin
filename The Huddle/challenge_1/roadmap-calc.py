24aeb3889d8b51f9d557e018a9afce98550a9c11
Jesus390 <jesusespinola90@gmail.com>
2025-06-25 22:25:30 -0300

    add - docs para las funciones

--- The Huddle/challenge_1/roadmap-calc.py
+++ The Huddle/challenge_1/roadmap-calc.py
@@ -24,28 +24,42 @@ emojis = {
 camino_principal = emojis['camino']['cuadrado_blanco']
 
 def crear_mapa(filas, columnas):
+    '''Creamos un mapa con emojis con filas y columnas'''
     return [[emojis['casa'][0] for _ in range(columnas)] for _ in range(filas)]
     
 def print_mapa(mapa):
+    '''Imprimimos el mapa con emojis'''
+
+    # cantidad_borde, longitud del mapa    
     cantidad_borde = int(len(mapa[0]))
+
+    # imprime borde del mapa
     print(f"+{'='*((cantidad_borde*3))}+")
     for row in mapa:
         print(f"|{''.join(row)} |")
+    # imprime borde del mapa
     print(f"+{'='*((cantidad_borde*3))}+")
 
 def crear_caminos_cuadricula(mapa):
+    '''Creamos un camino en el mapa'''
+
+    # x_filas, factor de escala
     if len(mapa) < 60:
         x_filas = .2
     else:
         x_filas = .3
 
+    # y_columnas, factor de escala
     if len(mapa[0]) < 60:
         x_columnas = .2
     else:
         x_columnas = .3
+
+    
     camino_filas = int(len(mapa)*x_filas)
     camino_columnas = int(len(mapa[0])*x_columnas)
     print(camino_filas, camino_columnas, len(mapa), len(mapa[0]))
+
     for i in range(len(mapa)):
         for j in range(len(mapa[0])):
             if i % camino_filas == 0 and j % camino_columnas == 0:
@@ -56,6 +70,8 @@ def crear_caminos_cuadricula(mapa):
                 mapa[i][j] = camino_principal
 
 def get_caminos(mapa):
+    '''retorna caminos disponibles'''
+
     caminos_disponibles = []
     cantidad = 0
     for i in range(len(mapa)):
@@ -66,6 +82,7 @@ def get_caminos(mapa):
     return caminos_disponibles, cantidad
 
 def agregar_obstaculos(mapa):
+    '''agrega obstaculos al mapa'''
     caminos_disponibles = get_caminos(mapa)
     cantidad_obstaculos = caminos_disponibles[1] * 0.04
     for _ in range(int(cantidad_obstaculos)):
@@ -74,6 +91,7 @@ def agregar_obstaculos(mapa):
         mapa[x[0]][x[1]] = random.choice(emojis['obstaculo'])
 
 def agregar_arbol(mapa):
+    '''agrega un arbol al mapa'''
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
             if mapa[i][j] == emojis['casa'][0]:
@@ -84,6 +102,7 @@ def agregar_arbol(mapa):
                     mapa[i][j] = random.choice(emojis['arbol'])
 
 def inicio(mapa):
+    '''agrega el punto de inicio al mapa'''
     while True:
         fila = int(input("Ingresa la fila donde deseas empezar: "))
         columna = int(input("Ingresa la columna donde deseas empezar: "))
@@ -94,6 +113,7 @@ def inicio(mapa):
         print("Posicion no disponible.")
 
 def final(mapa):
+    '''agrega el punto de fin al mapa'''
     while True:
         fila = int(input("Ingresa la fila donde deseas finalizar: "))
         columna = int(input("Ingresa la columna donde deseas finalizar: "))
@@ -104,6 +124,7 @@ def final(mapa):
         print("Posicion no disponible.")
     
 def get_nodos(mapa):
+    '''obtiene los nodos del mapa'''
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
             pass
