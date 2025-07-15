3d38b848605fdd6febcf6772e583e767df556fe6
Jesus390 <jesusespinola90@gmail.com>
2025-07-15 11:54:40 -0300

    se agrego excepciones al servidor

--- The Huddle/challenge_3/server.py
+++ The Huddle/challenge_3/server.py
@@ -1,21 +1,27 @@
 import socket
+import time
 
 HOST = 'localhost'
 PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Inicializando servidor...")
-    print(f"Host: {HOST}")
-    print(f"Porta: {PORT}")
     s.bind((HOST, PORT))
+    print(f"Servidor vinculado: {HOST}:{PORT}")
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Conectado a', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-
-        print("Cerrando servidor.")
+    print("Esperando Conecciones...")
+    while True:
+        try:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Cliente conectado: {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(f"{addr}-{time.ctime(time.time())}: {data.decode()}")
+                    conn.sendall(data)
+        except ConnectionResetError:
+            print(f"Conexión con {addr} cerrada")
+        except OSError as e:
+            print(f"Error de socket: {e}")
