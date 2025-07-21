898e411438051db6920508951512e6b7d12cbbbe
Jesus390 <jesusespinola90@gmail.com>
2025-07-20 21:50:55 -0300

    issue#91 - implementación de settimeout()

--- The Huddle/challenge_3/server.py
+++ The Huddle/challenge_3/server.py
@@ -10,13 +10,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(f"Servidor vinculado: {HOST}:{PORT}")
     s.listen()
     print("Esperando Conecciones...")
+    s.settimeout(3)
     while True:
-        conn, addr = s.accept()
-        while conn:
-            print(f"Conectado a {addr}")
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"Recebi: {data}")
-            conn.sendall(data)
+        try:
+            conn, addr = s.accept()
+            while conn:
+                print(f"Conectado a {addr}")
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"Recebi: {data}")
+                conn.sendall(data)
+        except socket.timeout:
+            print("Timeout")
     
